05531837827440b6d17e5d18a608d000a006e0df
cjlbelegendary <1325129305@qq.com>
2025-02-18 18:45:11 +0800

    '初始化'

---
+++ src/pages/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class='home'>慕课</div>
+  <span>{{ user.token }}</span>
+</template>
+
+<script lang='ts' setup name='Home'>
+  import {useUserStore} from '@/stores/user.js'
+  const user=useUserStore()
+</script>
+
+<style scoped>
+  
+</style>
